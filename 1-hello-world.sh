6fa60fd623300cb4e34cb04a080a0e609e3819d3
shabeena.syed <syedfarzana589@gmail.com>
2025-02-09 17:41:05 +0530

    arguments

--- 1-hello-world.sh
+++ 1-hello-world.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "pease enter username::"
-read USERNAME #user name is variable
-echo " please entr password :: "
-read PASSWARD
+read -s USERNAME #user name is variable
+echo "please entr password :: "
+read -s PASSWARD
