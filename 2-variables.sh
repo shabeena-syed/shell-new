d59da01f3933faa8e40c0ff495105d0e71f85e73
shabeena.syed <syedfarzana589@gmail.com>
2025-02-09 16:26:43 +0530

    arguments

---
+++ 2-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bin
+1= shabeena
+2= shabbu
+echo "hi $1"
+echo "hi $2"
+echo "how r u $1"
+echo "im gud $2"
---
+++ 3-arguments.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+name1= $1
+name2= $2
+
+echo "hi $name1"
+echo " hello $name2"
