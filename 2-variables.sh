76ab698feba944455e5db4a14d20022af8f7c133
shabeena.syed <syedfarzana589@gmail.com>
2025-02-19 13:11:16 +0530

    changes

--- 2-variables.sh
+++ 2-variables.sh
@@ -5,3 +5,8 @@ echo "hi $1"
 echo "hi $2"
 echo "how r u $1"
 echo "im gud $2"
+echo "thia script has to be printed"
+** if u want to chage the name u have to change everywhere **
+** so if u use varibles we can replace the values at the start of script **
+** we can define the variables at the top of the script and thn change whn unwanted **
+** variabls **== holds some values **
