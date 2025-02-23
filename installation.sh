f079c3f926cfae992f52f4dffea44d6ae58dfa72
shabeena.syed <syedfarzana589@gmail.com>
2025-02-23 22:32:15 +0530

    installations

---
+++ installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#if id is 0 super zero super user if not super user
+
+USERID=${id -u}
+if [ $USERID -eq 0]
+then 
+ echo "you are a super user"
+else 
+ echo "please run this script by super user"
+fi
+yum install mysql -y
