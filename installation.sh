e2fcfc08da90ecd9d5d3d91f61c94a852dbcbe78
shabeena.syed <syedfarzana589@gmail.com>
2025-02-25 16:05:50 +0530

    installation

--- installation.sh
+++ installation.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-
-#if id is 0 super zero super user if not super user
+# If user ID is 0, superuser; if not, not a superuser
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%s)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-VALIDATE(){
-  if [ $1 -ne 0 ]
-  then 
+
+# Validation function
+VALIDATE() {
+  if [ $1 -ne 0 ]; then 
     echo "$2..... failure"
     exit 1
   else
@@ -17,16 +17,18 @@ VALIDATE(){
   fi
 }
 
-if [ $USERID -eq 0 ]
-then
- echo"please run this script with root user"
+# Check if the script is being run by root
+if [ $USERID -eq 0 ]; then
+  echo "You are running this script as root user."
 else
- echo "you are super user"
+  echo "Please run this script as root user."
+  exit 1
 fi
 
-dnf install mysql -y &>>$LOGFILE
+# Install MySQL and log output
+dnf install mysql -y &>> $LOGFILE
+VALIDATE $? "Installing MySQL"
 
-VALIDATE $? "installing mysql"
-dnf install git -y &>>$LOGFILE
-VALIDATE $? "installing git" 
-
+# Install Git and log output
+dnf install git -y &>> $LOGFILE
+VALIDATE $? "Installing Git"
