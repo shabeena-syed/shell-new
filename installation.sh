c98b56a0f0533d1c96e76c1ee6982fec868bbdb2
shabeena.syed <syedfarzana589@gmail.com>
2025-02-25 14:15:39 +0530

    installation

--- installation.sh
+++ installation.sh
@@ -4,20 +4,14 @@
 #if id is 0 super zero super user if not super user
 
 USERID=$(id -u)
+validate(){
+    echo "exit status:$?"
+    echo "what are u doing:$1"
+}
 
 if [$USERID -eq 0]
-then 
- echo "you are a super user"
-fi 
 dnf install mysql -y
-if [ $? -ne 0 ]
-then
- echo "installation of mysql is.... failure"
- exit 1
-fi
+validate $? installing mysql
 dnf install git -y
-if [ $? -ne 0 ]
- echo " installation of git ....failure"
- exit 1
-fi 
+validate $? installing git 
 echo "is script proceeding?"
