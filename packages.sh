ce1ac6de61c65e8e89fd2e6664234622a8fb37bb
shabeena.syed <syedfarzana589@gmail.com>
2025-03-04 14:31:05 +0530

    installations

--- packages.sh
+++ packages.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%M-%S)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+VALIDATE() {
+  if [ $1 -ne 0 ]; then 
+    echo "$2..... failure"
+    exit 1
+  else
+    echo "$2......success"
+  fi
+}
+
 if [ $USERID -ne 0 ]
 then
   echo "you are not a super user"
@@ -11,5 +22,11 @@ fi
 for i in $@
 do
   echo "package to install: $i"
-  yum list installed $i 
+  yum list installed $i &>>$LOGFILE
+  if [ $? -eq 0 ]
+  then
+    echo "$i already installed...skipping"
+  else
+    echo "$i not installed.. need to install"
+  fi
 done
