c7bdfd48aa194ee347033a33f7718982d09f2fd5
shabeena.syed <syedfarzana589@gmail.com>
2025-03-06 18:12:56 +0530

    project

---
+++ backend.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ]
+then 
+ echo "please run this script with root user"
+  exit 1
+else
+  echo "you are a root user"
+fi
+
+yum module disable nodejs -y &>>LOGFILE
+VALIDATE $? "disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>LOGFILE
+VALIDATE $? "enabling nodejs new version:20"
+
+dnf install nodejs -y &>>LOGFILE
+VALIDATE $? "installing nodejs"
+
+useradd expense
+VALIDATE $? "creating user expense"
+
+
+
+
+
